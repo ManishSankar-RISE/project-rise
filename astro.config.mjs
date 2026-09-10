1a01c7ef807dada8f99740f8c83bc3fd0cac5562
ManishSankar-RISE <manishsankar2010@gmail.com>
2026-09-10 17:28:35 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://riseinitiatives.github.io', 
-	base: '/project-rise',  
+	base: '',  
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
